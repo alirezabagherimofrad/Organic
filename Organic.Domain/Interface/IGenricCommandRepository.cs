f1d0aa5704043d26e4684a5eadeb4a358c3e2342
mohammad zomorodian <smzomorodin@gmail.com>
2025-07-27 01:15:38 +0330

    Add refrence Infrastructure and Add IGenericQuery and command Repository

---
+++ Organic.Domain/Interface/IGenricCommandRepository.cs
@@ -0,0 +1,16 @@
+﻿using Organic.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Organic.Domain.Interface
+{
+    public interface IGenricCommandRepository<T> where T : class
+    {
+        Task<T> Add(T entity);
+        Task<bool> Update(T entity);
+        Task<bool> Delete(T entity);
+    }
+}
