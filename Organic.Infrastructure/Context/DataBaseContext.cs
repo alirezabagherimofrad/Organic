21a27b1b23fad0bbe885890f06f343bf0a5ddc02
alirezabagherimofrad <alirezabagherimofrad>
2025-07-26 14:14:31 +0330

    Add project files.

---
+++ Organic.Infrastructure/Context/DataBaseContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Organic.Infrastructure.Context
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext(){}
+
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
---
+++ Organic.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Organic.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
