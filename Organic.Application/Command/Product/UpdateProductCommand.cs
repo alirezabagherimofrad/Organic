4abc0ccdbe1074965509e2e44e1e08d9af1206bf
alirezabagherimofrad <alirezabagherimofrad>
2025-07-31 11:59:30 +0330

    Feat : DeleteProduct

---
+++ Organic.Application/Command/Product/UpdateProductCommand.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Organic.Application.Command.Product
+{
+    public class UpdateProductCommand : IRequest<bool>
+    {
+        public string Name { get; set; }
+
+        public int Price { get; set; }
+
+        public string? Description { get; set; }
+
+        public int Stock { get; set; }
+
+        public ICollection<IFormFile> ProductImages { get; set; } = new List<IFormFile>();
+    }
+}
